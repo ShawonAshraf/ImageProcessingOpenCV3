2ecca7cbe2612c8eee62e232a3b002fba0ca03fc
ShawonAshraf <shawon13@live.com>
2017-02-27 01:39:19 +0600

    Image manipulation, pixel by pixel

---
+++ Intro to OpenCV/Manipulating Image pixel by pixel/sources/manipulate_px.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+
+image = cv2.imread('../images/kids_field_bangladesh.jpg', cv2.IMREAD_UNCHANGED)
+print(image)
+
+threshold = np.amax(image)
+
+for x in np.nditer(image, op_flags=['readwrite']):
+    if x >= threshold / 2:
+        x[...] = 1
+    elif x < threshold / 2:
+        x[...] = 0
+
+print(image)
+
+cv2.imshow('binary image', image)
+
+key = cv2.waitKey(0)
+if key == 27 or ord('q'):
+    cv2.destroyAllWindows()
