fdb28efe29e2ed8d0093a7deb75ee467081055f4
ShawonAshraf <shawon13@live.com>
2017-03-11 23:54:30 +0600

    Added old codes to Face Rec

---
+++ Demo Scripts/ECE R&D/Face-Recognition-From-Image/scripts/face_rec.py
@@ -0,0 +1,36 @@
+import numpy
+import cv2
+
+
+def detect_faces(impath):
+    scale_factor = 1.1
+    mask = 7
+    min_size = (30, 30)
+
+    scale_f = cv2.CASCADE_SCALE_IMAGE
+
+    face_cascade = cv2.CascadeClassifier(
+        '../classifiers/haarcascade_frontalface_default.xml')
+
+    img = cv2.imread(impath)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # find faces as rectangles
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=scale_factor,
+                                          minNeighbors=mask,
+                                          minSize=min_size,
+                                          flags=scale_f)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imwrite('../dump/out.jpg', img)
+
+    return (img, faces)
+
+
+def show_result(img_tup):
+    cv2.imshow('img', img_tup[0])
+    # Window lasts for 30 seconds
+    cv2.waitKey(1000 * 30)
+    cv2.destroyAllWindows()
